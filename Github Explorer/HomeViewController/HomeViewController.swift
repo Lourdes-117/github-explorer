ff63bad76edb9fec20c98856b6b7737202636034
Lourdes Dinesh <lourdesdinesh.s@tringapps.com>
2021-07-06 10:37:44 +0530

    HomeViewController initial Setup

--- Github Explorer/HomeViewController/HomeViewController.swift
+++ Github Explorer/HomeViewController/HomeViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+// MARK: Outlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var noResultsView: UIView!
+    
+// MARK: Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initialSetup()
+    }
+    
+    private func initialSetup() {
+        noResultsView.isHidden = true
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-
+// MARK: Button Actions
+    @IBAction func onTapAddNewRepo(_ sender: Any) {
+    }
 }
 
+// MARK: TableView DataSource
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
+
+// MARK: TableView Delegate
+extension HomeViewController: UITableViewDelegate {
+    
+}
